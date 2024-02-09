4e13abe3afc2a0c9739d39a04aca0096d1e9387f
Colin Casey <casey.colin@gmail.com>
2024-02-09 16:33:35 -0400

    Aptfile parsing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+use crate::aptfile::Aptfile;
 use crate::errors::AptBuildpackError;
-use libcnb::build::{BuildContext, BuildResult};
-use libcnb::detect::{DetectContext, DetectResult};
+use libcnb::build::{BuildContext, BuildResult, BuildResultBuilder};
+use libcnb::detect::{DetectContext, DetectResult, DetectResultBuilder};
 use libcnb::generic::{GenericMetadata, GenericPlatform};
 use libcnb::{buildpack_main, Buildpack};
+use std::fs;
 
+mod aptfile;
 mod errors;
 
 buildpack_main!(AptBuildpack);
+
+const APTFILE_PATH: &str = "Aptfile";
 
 struct AptBuildpack;
 
@@ -15,11 +20,26 @@ impl Buildpack for AptBuildpack {
     type Metadata = GenericMetadata;
     type Error = AptBuildpackError;
 
-    fn detect(&self, _context: DetectContext<Self>) -> libcnb::Result<DetectResult, Self::Error> {
-        todo!()
+    fn detect(&self, context: DetectContext<Self>) -> libcnb::Result<DetectResult, Self::Error> {
+        let exists = context
+            .app_dir
+            .join(APTFILE_PATH)
+            .try_exists()
+            .map_err(AptBuildpackError::DetectAptfile)?;
+
+        if exists {
+            DetectResultBuilder::pass().build()
+        } else {
+            DetectResultBuilder::fail().build()
+        }
     }
 
-    fn build(&self, _context: BuildContext<Self>) -> libcnb::Result<BuildResult, Self::Error> {
-        todo!()
+    fn build(&self, context: BuildContext<Self>) -> libcnb::Result<BuildResult, Self::Error> {
+        let _aptfile: Aptfile = fs::read_to_string(context.app_dir.join(APTFILE_PATH))
+            .map_err(AptBuildpackError::ReadAptfile)?
+            .parse()
+            .map_err(|_| AptBuildpackError::ParseAptfile)?;
+
+        BuildResultBuilder::new().build()
     }
 }
