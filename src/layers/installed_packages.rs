3c67cb5d00ec14f89011ef413762fb194370fc3c
Colin Casey <casey.colin@gmail.com>
2024-02-12 12:43:46 -0400

    Installed packages layer

---
+++ src/layers/installed_packages.rs
@@ -0,0 +1,181 @@
+use crate::aptfile::Aptfile;
+use crate::AptBuildpack;
+use commons::output::interface::SectionLogger;
+use commons::output::section_log::log_step;
+use libcnb::build::BuildContext;
+use libcnb::data::buildpack::StackId;
+use libcnb::data::layer_content_metadata::LayerTypes;
+use libcnb::layer::{ExistingLayerStrategy, Layer, LayerData, LayerResult, LayerResultBuilder};
+use libcnb::layer_env::{LayerEnv, ModificationBehavior, Scope};
+use libcnb::Buildpack;
+use serde::{Deserialize, Serialize};
+use std::ffi::OsString;
+use std::path::{Path, PathBuf};
+use std::sync::atomic::{AtomicBool, Ordering};
+
+pub(crate) struct InstalledPackagesLayer<'a> {
+    pub(crate) aptfile: &'a Aptfile,
+    pub(crate) cache_restored: &'a AtomicBool,
+    pub(crate) _section_logger: &'a dyn SectionLogger,
+}
+
+impl<'a> Layer for InstalledPackagesLayer<'a> {
+    type Buildpack = AptBuildpack;
+    type Metadata = InstalledPackagesMetadata;
+
+    fn types(&self) -> LayerTypes {
+        LayerTypes {
+            build: true,
+            launch: true,
+            cache: true,
+        }
+    }
+
+    fn create(
+        &self,
+        context: &BuildContext<Self::Buildpack>,
+        layer_path: &Path,
+    ) -> Result<LayerResult<Self::Metadata>, <Self::Buildpack as Buildpack>::Error> {
+        log_step("Creating cache directory");
+
+        let mut env = LayerEnv::new();
+
+        let bin_paths = [
+            layer_path.join("bin"),
+            layer_path.join("usr/bin"),
+            layer_path.join("usr/sbin"),
+        ];
+        prepend_to_env_var(&mut env, "PATH", ":", &bin_paths);
+
+        let library_paths = [
+            layer_path.join("usr/lib/x86_64-linux-gnu"),
+            layer_path.join("usr/lib/i386-linux-gnu"),
+            layer_path.join("usr/lib"),
+            layer_path.join("lib/x86_64-linux-gnu"),
+            layer_path.join("lib/i386-linux-gnu"),
+            layer_path.join("lib"),
+        ];
+        prepend_to_env_var(&mut env, "LD_LIBRARY_PATH", ":", &library_paths);
+        prepend_to_env_var(&mut env, "LIBRARY_PATH", ":", &library_paths);
+
+        let include_paths = [
+            layer_path.join("usr/include/x86_64-linux-gnu"),
+            layer_path.join("usr/include/i386-linux-gnu"),
+            layer_path.join("usr/include"),
+        ];
+        prepend_to_env_var(&mut env, "INCLUDE_PATH", ":", &include_paths);
+        prepend_to_env_var(&mut env, "CPATH", ":", &include_paths);
+        prepend_to_env_var(&mut env, "CPPPATH", ":", &include_paths);
+
+        let pkg_config_paths = [
+            layer_path.join("usr/lib/x86_64-linux-gnu/pkgconfig"),
+            layer_path.join("usr/lib/i386-linux-gnu/pkgconfig"),
+            layer_path.join("usr/lib/pkgconfig"),
+        ];
+        prepend_to_env_var(&mut env, "PKG_CONFIG_PATH", ":", &pkg_config_paths);
+
+        LayerResultBuilder::new(InstalledPackagesMetadata::new(
+            self.aptfile.clone(),
+            context.stack_id.clone(),
+        ))
+        .env(env)
+        .build()
+    }
+
+    fn existing_layer_strategy(
+        &self,
+        context: &BuildContext<Self::Buildpack>,
+        layer_data: &LayerData<Self::Metadata>,
+    ) -> Result<ExistingLayerStrategy, <Self::Buildpack as Buildpack>::Error> {
+        let old_meta = &layer_data.content_metadata.metadata;
+        let new_meta =
+            &InstalledPackagesMetadata::new(self.aptfile.clone(), context.stack_id.clone());
+        if old_meta == new_meta {
+            log_step("Restoring installed packages");
+            self.cache_restored.store(true, Ordering::Relaxed);
+            Ok(ExistingLayerStrategy::Keep)
+        } else {
+            log_step(format!(
+                "Invalidating installed packages ({} changed)",
+                new_meta.changed_fields(old_meta).join(", ")
+            ));
+            Ok(ExistingLayerStrategy::Recreate)
+        }
+    }
+}
+
+fn prepend_to_env_var<I, T>(env: &mut LayerEnv, name: &str, separator: &str, paths: I)
+where
+    I: IntoIterator<Item = T>,
+    T: Into<OsString>,
+{
+    env.insert(Scope::All, ModificationBehavior::Delimiter, name, separator);
+    for path in paths {
+        env.insert(Scope::All, ModificationBehavior::Prepend, name, path.into());
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub(crate) struct InstalledPackagesMetadata {
+    stack_id: StackId,
+    aptfile: Aptfile,
+}
+
+impl InstalledPackagesMetadata {
+    pub(crate) fn new(aptfile: Aptfile, stack_id: StackId) -> Self {
+        Self { aptfile, stack_id }
+    }
+
+    pub(crate) fn changed_fields(&self, other: &InstalledPackagesMetadata) -> Vec<String> {
+        let mut changed_fields = vec![];
+        if self.stack_id != other.stack_id {
+            changed_fields.push("stack_id".to_string());
+        }
+        if self.aptfile != other.aptfile {
+            changed_fields.push("Aptfile".to_string());
+        }
+        changed_fields.sort();
+        changed_fields
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub(crate) enum InstalledPackagesState {
+    New(PathBuf),
+    Restored,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use libcnb::data::stack_id;
+    use std::str::FromStr;
+
+    #[test]
+    fn installed_packages_metadata_with_all_changed_fields() {
+        assert_eq!(
+            InstalledPackagesMetadata::new(
+                Aptfile::from_str("package-1").unwrap(),
+                stack_id!("heroku-20")
+            )
+            .changed_fields(&InstalledPackagesMetadata::new(
+                Aptfile::from_str("package-2").unwrap(),
+                stack_id!("heroku-22")
+            )),
+            &["Aptfile", "stack_id"]
+        );
+    }
+
+    #[test]
+    fn installed_packages_metadata_with_no_changed_fields() {
+        assert!(InstalledPackagesMetadata::new(
+            Aptfile::from_str("package-1").unwrap(),
+            stack_id!("heroku-20")
+        )
+        .changed_fields(&InstalledPackagesMetadata::new(
+            Aptfile::from_str("package-1").unwrap(),
+            stack_id!("heroku-20")
+        ))
+        .is_empty());
+    }
+}
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,1 +1,88 @@
+use libcnb::data::buildpack_id;
+use libcnb_test::{
+    assert_contains, assert_not_contains, BuildConfig, BuildpackReference, TestContext, TestRunner,
+};
+use std::path::PathBuf;
 
+#[test]
+#[ignore = "integration test"]
+fn test_cache_restored() {
+    apt_integration_test("./fixtures/basic", |ctx| {
+        assert_contains!(ctx.pack_stdout, "Heroku Apt Buildpack");
+        assert_contains!(ctx.pack_stdout, "Apt packages cache");
+        assert_contains!(ctx.pack_stdout, "Creating cache directory");
+
+        let config = ctx.config.clone();
+        ctx.rebuild(config, |ctx| {
+            assert_not_contains!(ctx.pack_stdout, "Creating cache directory");
+            assert_contains!(ctx.pack_stdout, "Restoring installed packages");
+        });
+    });
+}
+
+#[test]
+#[ignore = "integration test"]
+fn test_cache_invalidated_when_aptfile_changes() {
+    apt_integration_test("./fixtures/basic", |ctx| {
+        assert_contains!(ctx.pack_stdout, "Heroku Apt Buildpack");
+        assert_contains!(ctx.pack_stdout, "Apt packages cache");
+        assert_contains!(ctx.pack_stdout, "Creating cache directory");
+
+        let mut config = ctx.config.clone();
+        config.app_dir_preprocessor(|app_dir| {
+            std::fs::write(app_dir.join("Aptfile"), "").unwrap();
+        });
+        ctx.rebuild(config, |ctx| {
+            assert_contains!(
+                ctx.pack_stdout,
+                "Invalidating installed packages (Aptfile changed)"
+            );
+            assert_contains!(ctx.pack_stdout, "Creating cache directory");
+        });
+    });
+}
+
+const DEFAULT_BUILDER: &str = "heroku/builder:22";
+
+fn get_integration_test_builder() -> String {
+    std::env::var("INTEGRATION_TEST_CNB_BUILDER").unwrap_or(DEFAULT_BUILDER.to_string())
+}
+
+fn apt_integration_test(fixture: &str, test_body: fn(TestContext)) {
+    apt_integration_test_with_config(fixture, |_| {}, test_body);
+}
+
+fn apt_integration_test_with_config(
+    fixture: &str,
+    with_config: fn(&mut BuildConfig),
+    test_body: fn(TestContext),
+) {
+    integration_test_with_config(
+        fixture,
+        with_config,
+        test_body,
+        &[BuildpackReference::WorkspaceBuildpack(buildpack_id!(
+            "heroku/apt"
+        ))],
+    );
+}
+
+fn integration_test_with_config(
+    fixture: &str,
+    with_config: fn(&mut BuildConfig),
+    test_body: fn(TestContext),
+    buildpacks: &[BuildpackReference],
+) {
+    let cargo_manifest_dir = std::env::var("CARGO_MANIFEST_DIR")
+        .map(PathBuf::from)
+        .expect("The CARGO_MANIFEST_DIR should be automatically set by Cargo when running tests but it was not");
+
+    let builder = get_integration_test_builder();
+    let app_dir = cargo_manifest_dir.join("tests").join(fixture);
+
+    let mut build_config = BuildConfig::new(builder, app_dir);
+    build_config.buildpacks(buildpacks);
+    with_config(&mut build_config);
+
+    TestRunner::default().build(build_config, test_body);
+}
