heroku/buildpacks-deb-packages

commit 1720c2b3143fdce8408b732c57dcd401cff1a4b0

Use function print interface (#115)
* Update to latest detect style (and global)

Guidance is here:

https://github.com/heroku-buildpacks/bullet_stream/blob/fbfa7b377542eb6a550861a525cac03ca61c7180/examples/style_guide.rs#L21-L36

* Use global "all done" message

* Global header print

* Update method return type

* Remove logging variable

* Replace streaming interface with functions

* Replace streaming interface with functions

* Update bullet_stream

* Use function style output API

* Use function style output API

* Remove unused argument from function

* Use function interface

* Update log interface, change function args

* Change return signature

* Prefer function interface

* Prefer function interface

* Prefer function interface

* Prefer function interface

* Prefer function interface

* Prefer function interface

* Prefer function interface

* Prefer function interface

* Prefer function interface

* Update imports

* Integration tests stdout -> stderr

* Fix integration test

The python buildpack is still printing to stdout, so this test fails to find the expected line. Combining stdout and stderr for the assertion fixes the tests.

* Prefer function interface

* Update function args

* Fix error logging tests

* Remove unused crate

* Use bullet_stream WIP test feature

This feature globally blocks all other threads from modifying the writer for the closure's duration. The prior thread_local based logger has a race condition when "paragraph" text is being used and many tests are calling `global::set_version` as every time that is called, the state of the text that was just written is lost (this is used to track whether or not we need a newline).

This commit validates usage of capturing build output in tests. Once all maintainers are satisfied with the feature, it will be released and we can update this repo to use the released version.

* Use stdout+stderr in tests

src/create_package_index.rs
@@ -8,8 +8,7 @@ use crate::{BuildpackResult, DebianPackagesBuildpack, DebianPackagesBuildpackErr
 use apt_parser::errors::APTError;
 use apt_parser::Release;
 use async_compression::tokio::bufread::GzipDecoder;
-use bullet_stream::state::Bullet;
-use bullet_stream::{style, Print};
+use bullet_stream::{global::print, style};
 use futures::io::AllowStdIo;
 use futures::TryStreamExt;
 use libcnb::build::BuildContext;
@@ -28,7 +27,6 @@ use sequoia_openpgp::Cert;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 use std::fmt::{Display, Formatter};
-use std::io::Stdout;
 use std::path::PathBuf;
 use std::str::FromStr;
 use std::sync::Arc;
@@ -48,75 +46,62 @@ pub(crate) async fn create_package_index(
     context: &Arc<BuildContext<DebianPackagesBuildpack>>,
     client: &ClientWithMiddleware,
     source_list: &[Source],
-    log: Print<Bullet<Stdout>>,
-) -> BuildpackResult<(PackageIndex, Print<Bullet<Stdout>>)> {
-    let log = log.h2("Creating package index");
+) -> BuildpackResult<PackageIndex> {
+    print::header("Creating package index");
 
-    let log = source_list
-        .iter()
-        .fold(log.bullet("Package sources"), |log, source| {
-            source.suites.iter().fold(log, |log, suite| {
-                log.sub_bullet(format!(
-                    "{repository_uri} {suite} [{components}]",
-                    repository_uri = style::url(&source.uri),
-                    components = source.components.join(", "),
-                ))
-            })
+    for source in source_list {
+        print::bullet("Package sources");
+        for suite in &source.suites {
+            print::sub_bullet(format!(
+                "{repository_uri} {suite} [{components}]",
+                repository_uri = style::url(&source.uri),
+                components = source.components.join(", "),
+            ));
+        }
+    }
+
+    let timer = print::sub_start_timer("Updating");
+    let updated_sources = update_sources(context, client, source_list).await?;
+    timer.done();
+
+    for updated_source in &updated_sources {
+        print::sub_bullet(match &updated_source.release_file.cache_state {
+            UpdatedSourceCacheState::Cached => format!(
+                "Restored release file from cache {url}",
+                url = style::details(style::url(&updated_source.release_file.release_file_url))
+            ),
+            UpdatedSourceCacheState::New => format!(
+                "Downloaded release file {url}",
+                url = style::url(&updated_source.release_file.release_file_url)
+            ),
+            UpdatedSourceCacheState::Invalidated(reason) => format!(
+                "Redownloaded release file {url} {reason}",
+                url = style::url(&updated_source.release_file.release_file_url),
+                reason = style::details(reason)
+            ),
         });
 
-    let timer = log.start_timer("Updating");
-    let updated_sources = update_sources(context, client, source_list).await?;
-    let log = timer.done();
+        for updated_package_index in &updated_source.package_indexes {
+            print::sub_bullet(match &updated_package_index.cache_state {
+                UpdatedSourceCacheState::Cached => format!(
+                    "Restored package index from cache {url}",
+                    url = style::details(style::url(&updated_package_index.package_index_url))
+                ),
+                UpdatedSourceCacheState::New => format!(
+                    "Downloaded package index {url}",
+                    url = style::url(&updated_package_index.package_index_url)
+                ),
+                UpdatedSourceCacheState::Invalidated(reason) => format!(
+                    "Redownloaded package index {url} {reason}",
+                    url = style::url(&updated_package_index.package_index_url),
+                    reason = style::details(reason)
+                ),
+            });
+        }
+    }
 
-    let log = updated_sources
-        .iter()
-        .fold(log, |log, updated_source| {
-            let update_source_log =
-                log.sub_bullet(match &updated_source.release_file.cache_state {
-                    UpdatedSourceCacheState::Cached => format!(
-                        "Restored release file from cache {url}",
-                        url = style::details(style::url(
-                            &updated_source.release_file.release_file_url
-                        ))
-                    ),
-                    UpdatedSourceCacheState::New => format!(
-                        "Downloaded release file {url}",
-                        url = style::url(&updated_source.release_file.release_file_url)
-                    ),
-                    UpdatedSourceCacheState::Invalidated(reason) => format!(
-                        "Redownloaded release file {url} {reason}",
-                        url = style::url(&updated_source.release_file.release_file_url),
-                        reason = style::details(reason)
-                    ),
-                });
-
-            updated_source.package_indexes.iter().fold(
-                update_source_log,
-                |update_source_log, updated_package_index| {
-                    update_source_log.sub_bullet(match &updated_package_index.cache_state {
-                        UpdatedSourceCacheState::Cached => format!(
-                            "Restored package index from cache {url}",
-                            url = style::details(style::url(
-                                &updated_package_index.package_index_url
-                            ))
-                        ),
-                        UpdatedSourceCacheState::New => format!(
-                            "Downloaded package index {url}",
-                            url = style::url(&updated_package_index.package_index_url)
-                        ),
-                        UpdatedSourceCacheState::Invalidated(reason) => format!(
-                            "Redownloaded package index {url} {reason}",
-                            url = style::url(&updated_package_index.package_index_url),
-                            reason = style::details(reason)
-                        ),
-                    })
-                },
-            )
-        })
-        .done();
-
-    let log = log.bullet("Building package index");
-    let timer = log.start_timer("Processing package files");
+    print::bullet("Building package index");
+    let timer = print::sub_start_timer("Processing package files");
     let package_index = build_package_index(
         updated_sources
             .into_iter()
@@ -124,20 +109,18 @@ pub(crate) async fn create_package_index(
             .collect(),
     )
     .await?;
-    let log = timer.done();
+    timer.done();
 
     info!(
         { PACKAGE_INDEX_SIZE } = package_index.packages_indexed,
         "package index"
     );
-    let log = log
-        .sub_bullet(format!(
-            "Indexed {} packages",
-            package_index.packages_indexed
-        ))
-        .done();
+    print::sub_bullet(format!(
+        "Indexed {} packages",
+        package_index.packages_indexed
+    ));
 
-    Ok((package_index, log))
+    Ok(package_index)
 }
 
 #[instrument(skip_all)]

src/determine_packages_to_install.rs
@@ -2,15 +2,13 @@ use crate::config::RequestedPackage;
 use crate::debian::{PackageIndex, RepositoryPackage};
 use crate::{BuildpackResult, DebianPackagesBuildpackError};
 use apt_parser::Control;
-use bullet_stream::state::Bullet;
-use bullet_stream::{style, Print};
+use bullet_stream::{global::print, style};
 use edit_distance::edit_distance;
 use indexmap::IndexSet;
 use serde::Serialize;
 use std::collections::HashSet;
 use std::fmt::{Display, Formatter};
 use std::fs::read_to_string;
-use std::io::Stdout;
 use std::path::PathBuf;
 use tracing::instrument;
 
@@ -18,11 +16,9 @@ use tracing::instrument;
 pub(crate) fn determine_packages_to_install(
     package_index: &PackageIndex,
     requested_packages: IndexSet<RequestedPackage>,
-    mut log: Print<Bullet<Stdout>>,
-) -> BuildpackResult<(Vec<RepositoryPackage>, Print<Bullet<Stdout>>)> {
-    log = log.h2("Determining packages to install");
-
-    let sub_bullet = log.bullet("Collecting system install information");
+) -> BuildpackResult<Vec<RepositoryPackage>> {
+    print::header("Determining packages to install");
+    print::sub_bullet("Collecting system install information");
     let system_packages_path = PathBuf::from("/var/lib/dpkg/status");
     let system_packages = read_to_string(&system_packages_path)
         .map_err(|e| {
@@ -42,12 +38,11 @@ pub(crate) fn determine_packages_to_install(
                 .map(SystemPackage::from)
         })
         .collect::<Result<IndexSet<_>, _>>()?;
-    log = sub_bullet.done();
 
     let mut packages_marked_for_install = IndexSet::new();
 
     for requested_package in requested_packages {
-        let mut notification_log = log.bullet(format!(
+        print::bullet(format!(
             "Determining install requirements for requested package {package}",
             package = style::value(requested_package.name.as_str())
         ));
@@ -66,14 +61,12 @@ pub(crate) fn determine_packages_to_install(
         )?;
 
         if package_notifications.is_empty() {
-            notification_log = notification_log.sub_bullet("Nothing to add");
+            print::sub_bullet("Nothing to add");
         } else {
             for package_notification in package_notifications {
-                notification_log = notification_log.sub_bullet(package_notification.to_string());
+                print::sub_bullet(package_notification.to_string());
             }
         }
-
-        log = notification_log.done();
     }
 
     let packages_to_install = packages_marked_for_install
@@ -81,7 +74,7 @@ pub(crate) fn determine_packages_to_install(
         .map(|package_marked_for_install| package_marked_for_install.repository_package)
         .collect::<Vec<_>>();
 
-    Ok((packages_to_install, log))
+    Ok(packages_to_install)
 }
 
 // NOTE: Since this buildpack is not meant to be a replacement for a fully-featured dependency

Cargo.toml
@@ -12,7 +12,7 @@ apt-parser = "1"
 ar = "0.9"
 async-compression = { version = "0.4", default-features = false, features = ["tokio", "gzip", "zstd", "xz"] }
 bon = "3"
-bullet_stream = "0.8"
+bullet_stream = ">=0.9, <1.0"
 const_format = "0.2"
 debversion = "0.4"
 edit-distance = "2"
@@ -39,7 +39,6 @@ walkdir = "2"
 [dev-dependencies]
 libcnb-test = "=0.29.0"
 regex = "1"
-strip-ansi-escapes = "0.2"
 tempfile = "3"
 
 [lints.rust]

src/errors.rs
@@ -6,26 +6,19 @@ use crate::errors::ErrorType::{Framework, Internal, UserFacing};
 use crate::install_packages::InstallPackagesError;
 use crate::{DebianPackagesBuildpackError, DetectError};
 use bon::builder;
-use bullet_stream::{style, Print};
+use bullet_stream::{global::print, style};
 use indoc::{formatdoc, indoc};
 use libcnb::Error;
 use std::collections::BTreeSet;
-use std::io::Write;
 use std::path::Path;
 
 const BUILDPACK_NAME: &str = "Heroku .deb Packages buildpack";
 
-pub(crate) fn on_error<W>(error: Error<DebianPackagesBuildpackError>, writer: W)
-where
-    W: Write + Sync + Send + 'static,
-{
-    print_error(
-        match error {
-            Error::BuildpackError(e) => on_buildpack_error(e),
-            e => on_framework_error(&e),
-        },
-        writer,
-    );
+pub(crate) fn on_error(error: Error<DebianPackagesBuildpackError>) {
+    print_error(match error {
+        Error::BuildpackError(e) => on_buildpack_error(e),
+        e => on_framework_error(&e),
+    });
 }
 
 fn on_buildpack_error(error: DebianPackagesBuildpackError) -> ErrorMessage {
@@ -846,18 +839,12 @@ fn create_error(
     }
 }
 
-fn print_error<W>(error_message: ErrorMessage, writer: W)
-where
-    W: Write + Send + Sync + 'static,
-{
-    let mut log = Print::new(writer).without_header();
+fn print_error(error_message: ErrorMessage) {
     if let Some(debug_info) = error_message.debug_info {
-        log = log
-            .bullet(style::important("Debug Info:"))
-            .sub_bullet(debug_info)
-            .done();
+        print::bullet(style::important("Debug Info:"));
+        print::sub_bullet(debug_info);
     }
-    log.error(error_message.message);
+    print::error(error_message.message);
 }
 
 fn file_value(value: impl AsRef<Path>) -> String {
@@ -905,6 +892,7 @@ mod tests {
     };
     use crate::DebianPackagesBuildpackError::UnsupportedDistro;
     use anyhow::anyhow;
+    use bullet_stream::strip_ansi;
     use libcnb::data::layer::LayerNameError;
     use libcnb_test::assert_contains_match;
     use std::collections::HashSet;
@@ -2530,11 +2518,11 @@ mod tests {
         error: impl Into<Error<DebianPackagesBuildpackError>>,
         assert_fn: impl FnOnce(String),
     ) {
-        let file = tempfile::NamedTempFile::new().unwrap();
-        let reader = file.reopen().unwrap();
-        let writer = strip_ansi_escapes::Writer::new(file);
-        on_error(error.into(), writer);
-        let actual_text = std::io::read_to_string(reader).unwrap();
+        let output = bullet_stream::global::with_locked_writer(Vec::new(), || {
+            on_error(error.into());
+        });
+
+        let actual_text = strip_ansi(String::from_utf8_lossy(&output));
         assert_fn(actual_text);
     }
 

src/install_packages.rs
@@ -6,8 +6,7 @@ use crate::{
 };
 use ar::Archive as ArArchive;
 use async_compression::tokio::bufread::{GzipDecoder, XzDecoder, ZstdDecoder};
-use bullet_stream::state::{Bullet, SubBullet};
-use bullet_stream::{style, Print};
+use bullet_stream::{global::print, style};
 use futures::io::AllowStdIo;
 use futures::TryStreamExt;
 use indexmap::IndexSet;
@@ -25,7 +24,7 @@ use std::collections::HashMap;
 use std::env::temp_dir;
 use std::ffi::OsString;
 use std::fs::File;
-use std::io::{Stdout, Write};
+use std::io::Write;
 use std::os::unix::ffi::OsStringExt;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
@@ -44,9 +43,8 @@ pub(crate) async fn install_packages(
     client: &ClientWithMiddleware,
     distro: &Distro,
     packages_to_install: Vec<RepositoryPackage>,
-    mut log: Print<Bullet<Stdout>>,
-) -> BuildpackResult<Print<Bullet<Stdout>>> {
-    log = log.h2("Installing packages");
+) -> BuildpackResult<()> {
+    print::header("Installing packages");
 
     let new_metadata = InstallationMetadata {
         package_checksums: packages_to_install
@@ -74,45 +72,38 @@ pub(crate) async fn install_packages(
 
     match install_layer.state {
         LayerState::Restored { .. } => {
-            log = packages_to_install
-                .iter()
-                .fold(
-                    log.bullet("Restoring packages from cache"),
-                    |log, package_to_install| {
-                        log.sub_bullet(style::value(format!(
-                            "{name}@{version}",
-                            name = package_to_install.name,
-                            version = package_to_install.version
-                        )))
-                    },
-                )
-                .done();
+            print::bullet("Restoring packages from cache");
+            for package_to_install in &packages_to_install {
+                print::sub_bullet(style::value(format!(
+                    "{name}@{version}",
+                    name = package_to_install.name,
+                    version = package_to_install.version
+                )));
+            }
         }
         LayerState::Empty { cause } => {
-            let install_log = packages_to_install.iter().fold(
-                log.bullet(match cause {
-                    EmptyLayerCause::NewlyCreated => "Requesting packages",
-                    EmptyLayerCause::InvalidMetadataAction { .. } => {
-                        "Requesting packages (invalid metadata)"
-                    }
-                    EmptyLayerCause::RestoredLayerAction { .. } => {
-                        "Requesting packages (packages changed)"
-                    }
-                }),
-                |log, package_to_install| {
-                    log.sub_bullet(format!(
-                        "{name_with_version} from {url}",
-                        name_with_version = style::value(format!(
-                            "{name}@{version}",
-                            name = package_to_install.name,
-                            version = package_to_install.version
-                        )),
-                        url = style::url(build_download_url(package_to_install))
-                    ))
-                },
-            );
+            print::bullet(match cause {
+                EmptyLayerCause::NewlyCreated => "Requesting packages",
+                EmptyLayerCause::InvalidMetadataAction { .. } => {
+                    "Requesting packages (invalid metadata)"
+                }
+                EmptyLayerCause::RestoredLayerAction { .. } => {
+                    "Requesting packages (packages changed)"
+                }
+            });
+            for package_to_install in &packages_to_install {
+                print::sub_bullet(format!(
+                    "{name_with_version} from {url}",
+                    name_with_version = style::value(format!(
+                        "{name}@{version}",
+                        name = package_to_install.name,
+                        version = package_to_install.version
+                    )),
+                    url = style::url(build_download_url(package_to_install))
+                ));
+            }
 
-            let timer = install_log.start_timer("Downloading");
+            let timer = print::sub_start_timer("Downloading");
             install_layer.write_metadata(new_metadata)?;
 
             let mut download_and_extract_handles = JoinSet::new();
@@ -134,7 +125,7 @@ pub(crate) async fn install_packages(
                 download_and_extract_handle.map_err(InstallPackagesError::TaskFailed)??;
             }
 
-            log = timer.done().done();
+            timer.done();
         }
     }
 
@@ -147,38 +138,34 @@ pub(crate) async fn install_packages(
 
     rewrite_package_configs(&install_layer.path()).await?;
 
-    let mut install_log = log.bullet("Installation complete");
+    print::bullet("Installation complete");
     if is_buildpack_debug_logging_enabled() {
-        install_log = print_layer_contents(&install_layer.path(), install_log);
+        print_layer_contents(&install_layer.path());
     }
-    log = install_log.done();
 
-    Ok(log)
+    Ok(())
 }
 
-fn print_layer_contents(
-    install_path: &Path,
-    log: Print<SubBullet<Stdout>>,
-) -> Print<SubBullet<Stdout>> {
-    let mut directory_log = log.start_stream("Layer file listing");
-    WalkDir::new(install_path)
-        .into_iter()
-        .flatten()
-        .filter(|entry| {
-            // filter out the env layer that's created for CNB environment files
-            if let Some(parent) = entry.path().parent() {
-                if parent == install_path.join("env") {
-                    return false;
+fn print_layer_contents(install_path: &Path) {
+    print::sub_stream_with("Layer file listing", |mut directory_log, _| {
+        WalkDir::new(install_path)
+            .into_iter()
+            .flatten()
+            .filter(|entry| {
+                // filter out the env layer that's created for CNB environment files
+                if let Some(parent) = entry.path().parent() {
+                    if parent == install_path.join("env") {
+                        return false;
+                    }
                 }
-            }
-            entry.file_type().is_file()
-        })
-        .map(|entry| entry.path().to_path_buf())
-        .for_each(|path| {
-            let _ = writeln!(&mut directory_log, "{}", path.to_string_lossy());
-        });
-    let _ = writeln!(&mut directory_log);
-    directory_log.done()
+                entry.file_type().is_file()
+            })
+            .map(|entry| entry.path().to_path_buf())
+            .for_each(|path| {
+                let _ = writeln!(&mut directory_log, "{}", path.to_string_lossy());
+            });
+        let _ = writeln!(&mut directory_log);
+    });
 }
 
 #[instrument(skip_all)]

src/main.rs
@@ -6,7 +6,7 @@ use crate::determine_packages_to_install::{
 };
 use crate::install_packages::{install_packages, InstallPackagesError};
 use crate::o11y::*;
-use bullet_stream::{style, Print};
+use bullet_stream::{global::print, style};
 use indoc::formatdoc;
 use libcnb::build::{BuildContext, BuildResult, BuildResultBuilder};
 use libcnb::detect::{DetectContext, DetectResult, DetectResultBuilder};
@@ -18,7 +18,6 @@ use reqwest_retry::policies::ExponentialBackoff;
 use reqwest_retry::RetryTransientMiddleware;
 use reqwest_tracing::{SpanBackendWithUrl, TracingMiddleware};
 use std::fmt::Debug;
-use std::io::stdout;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use std::time::Duration;
@@ -50,13 +49,12 @@ impl Buildpack for DebianPackagesBuildpack {
     type Error = DebianPackagesBuildpackError;
 
     fn detect(&self, context: DetectContext<Self>) -> libcnb::Result<DetectResult, Self::Error> {
-        let log = Print::new(stdout()).without_header();
         if let Some(project_toml) = get_project_toml(&context.app_dir)? {
             info!({ PROJECT_TOML_DETECTED } = true);
             if BuildpackConfig::is_present(project_toml)? {
                 DetectResultBuilder::pass().build()
             } else {
-                log.important("project.toml found, but no [com.heroku.buildpacks.deb-packages] configuration present.").done();
+                print::plain("project.toml found, but no [com.heroku.buildpacks.deb-packages] configuration present.");
                 info!({ PROJECT_TOML_NO_CONFIG } = true);
                 DetectResultBuilder::fail().build()
             }
@@ -66,7 +64,7 @@ impl Buildpack for DebianPackagesBuildpack {
             info!({ APTFILE_DETECTED } = true);
             DetectResultBuilder::pass().build()
         } else {
-            log.important("No project.toml or Aptfile found.").done();
+            print::plain("No project.toml or Aptfile found.");
             DetectResultBuilder::fail().build()
         }
     }
@@ -96,7 +94,7 @@ impl Buildpack for DebianPackagesBuildpack {
             .build()
             .expect("Should be able to construct the Async Runtime");
 
-        let mut log = Print::new(stdout()).h1(format!(
+        let started = print::buildpack(format!(
             "{buildpack_name} (v{buildpack_version})",
             buildpack_name = context
                 .buildpack_descriptor
@@ -108,7 +106,7 @@ impl Buildpack for DebianPackagesBuildpack {
         ));
 
         if get_aptfile(&context.app_dir)?.is_some() {
-            log = log.important(migrate_from_aptfile_help_message());
+            print::plain(style::important(migrate_from_aptfile_help_message()));
             // If we passed detect from the Aptfile but there is no project.toml then
             // print the warning and exit early.
             if get_project_toml(&context.app_dir)?.is_none() {
@@ -121,7 +119,8 @@ impl Buildpack for DebianPackagesBuildpack {
 
         if config.install.is_empty() {
             info!({ EARLY_EXIT_REASON } = "nothing_to_install", "early exit");
-            log.important(empty_config_help_message()).done();
+
+            print::plain(style::important(empty_config_help_message()));
             return BuildResultBuilder::new().build();
         }
 
@@ -139,36 +138,32 @@ impl Buildpack for DebianPackagesBuildpack {
             "configuration"
         );
 
-        log = log
-            .bullet("Distribution Info")
-            .sub_bullet(format!("Name: {}", &distro.name))
-            .sub_bullet(format!("Version: {}", &distro.version))
-            .sub_bullet(format!("Codename: {}", &distro.codename))
-            .sub_bullet(format!("Architecture: {}", &distro.architecture))
-            .done();
+        print::bullet("Distribution Info");
+        print::sub_bullet(format!("Name: {}", &distro.name));
+        print::sub_bullet(format!("Version: {}", &distro.version));
+        print::sub_bullet(format!("Codename: {}", &distro.codename));
+        print::sub_bullet(format!("Architecture: {}", &distro.architecture));
 
-        let (package_index, log) =
-            runtime.block_on(create_package_index(&context, &client, &source_list, log))?;
+        let package_index =
+            runtime.block_on(create_package_index(&context, &client, &source_list))?;
 
-        let (packages_to_install, log) =
-            determine_packages_to_install(&package_index, config.install, log)?;
+        let packages_to_install = determine_packages_to_install(&package_index, config.install)?;
 
-        let log = runtime.block_on(install_packages(
+        runtime.block_on(install_packages(
             &context,
             &client,
             &distro,
             packages_to_install,
-            log,
         ))?;
 
-        log.done();
+        print::all_done(&Some(started));
 
         BuildResultBuilder::new().build()
     }
 
     fn on_error(&self, error: libcnb::Error<Self::Error>) {
         error!({ ERROR } = ?error);
-        errors::on_error(error, stdout());
+        errors::on_error(error);
     }
 }
 

commit 79ea7d805ae2d87e1875d28a9f8d88a1a48bc6c3

Update logging style to fix issues introduced in #115

src/create_package_index.rs
@@ -49,8 +49,8 @@ pub(crate) async fn create_package_index(
 ) -> BuildpackResult<PackageIndex> {
     print::header("Creating package index");
 
+    print::bullet("Package sources");
     for source in source_list {
-        print::bullet("Package sources");
         for suite in &source.suites {
             print::sub_bullet(format!(
                 "{repository_uri} {suite} [{components}]",

src/determine_packages_to_install.rs
@@ -18,7 +18,7 @@ pub(crate) fn determine_packages_to_install(
     requested_packages: IndexSet<RequestedPackage>,
 ) -> BuildpackResult<Vec<RepositoryPackage>> {
     print::header("Determining packages to install");
-    print::sub_bullet("Collecting system install information");
+    print::bullet("Collecting system install information");
     let system_packages_path = PathBuf::from("/var/lib/dpkg/status");
     let system_packages = read_to_string(&system_packages_path)
         .map_err(|e| {